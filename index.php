9f35db25a88635157b2a6c1b2065612670bcdb15
Mrahh <emretugrul02@gmail.com>
2024-02-23 13:30:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Food Ordering System</title>
+</head>
+<body>
+	<a href="staff">Staff Management</a><br>
+	<a href="admin">Admin Management</a><br>
+</body>
+</html>
